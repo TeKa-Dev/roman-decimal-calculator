53bae1831dd621cf7d745693d2c63fc883458f5f
TeKa-Dev <teka.proger@gmail.com>
2022-05-23 00:54:13 +0500

    Update Main.java

--- src/org/teka/Main.java
+++ src/org/teka/Main.java
@@ -4,100 +4,108 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static final String[] operators = {"\\+", "-", "\\*", "/"};
-    private static final String[] ramanNums =
-            {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "C"};
+    private static final char[] operators = {'+', '-', '*', '/'};
+    private static final int[] values =       {100, 90, 50, 40,  10,  9,   5,   4,  1 };
+    private static final String[] romanNums = {"C","XC","L","XL","X","IX","V","IV","I"};
+    private static final String[] excMessages = {
+            "строка не является математической операцией",
+            "используются одновременно разные системы счисления",
+            "формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)",
+            "калькулятор должен принимать на вход числа от 1 до 10 включительно, не более",
+            "в римской системе нет отрицательных чисел и нуля"
+    };
 
     public static void main(String[] args) throws Exception {
-        System.out.println(
-                calc(new Scanner(System.in).nextLine()));
+        System.out.println(calc(new Scanner(System.in).nextLine()));
     }
 
+    /**
+     * Основной метод калькулятор
+     **/
     public static String calc(String input) throws Exception {
 
-        int a;
-        int b;
         int result;
-        boolean isRomanA;
-        boolean isRomanB;
-        int indexOpr = getIndexOpr(input);                                // ищем во входящем выражении математический оператор
+        char operator = '!';
+        boolean isRoman;
 
-        if (indexOpr == -1) {                                             // если не нашли мат. оператор то бросаем исключение
-            throw new Exception("строка не является математической операцией");
+        // ищем во входящем тексте математический оператор
+        for (char op : operators) {
+            if (input.contains(String.valueOf(op))) operator = op;
         }
-        else {                                                            // если нашли то
-            String[] nums = input.trim().split(operators[indexOpr]);      // делим выражение по оператору
-                                                                          // если больше двух операндов или есть еще оператор(ы) то бросаем исключение
-            if (nums.length != 2 || getIndexOpr(nums[0]) != -1 || getIndexOpr(nums[1]) != -1) {
-                throw new Exception("формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
-            } else {
+        // если не нашли мат. оператор то бросаем исключение
+        if (operator == '!') throw new Exception(excMessages[0]);
 
-                isRomanA = romToAr(nums[0].trim()) != 0;         // проверяем является ли первое число римским
-                isRomanB = romToAr(nums[1].trim()) != 0;         // проверяем является ли второе число римским
+        else {
+            // если нашли то делим выражение по оператору убирая пробелы и присваиваем
+            final String A = input.substring(0, input.indexOf(operator)).trim();
+            final String B = input.substring(input.indexOf(operator) + 1).trim();
 
-                if (isRomanA && isRomanB) {
-                    a = romToAr(nums[0].trim());                 // если оба римские, то конвертируем в арабские и присваеваем переменным
-                    b = romToAr(nums[1].trim());
-                }
-                else if (!isRomanA && !isRomanB) {              // если же оба арабские, то присваеваем через парсинг
-                    a = Integer.parseInt(nums[0].trim());
-                    b = Integer.parseInt(nums[1].trim());
-                }                                               // иначе получается, что один операнд римский а другой нет, бросаем исключение
-                else throw new Exception("используются одновременно разные системы счисления");
+            // пробуем конвертировать строку в арабские числа, если возвращается 0, то числа были не римские
+            int a = romanToDecimal(A);
+            int b = romanToDecimal(B);
+            if (a != 0 && b != 0) isRoman = true;
 
-                if (a > 10 && b > 10)                           // проверяем чтобы числа на входе были не больше 10, если меньше то считаем
-                    throw new Exception("Калькулятор должен принимать на вход числа от 1 до 10 включительно, не более");
-                else if (indexOpr == 0) result = a + b;
-                else if (indexOpr == 1) result = a - b;
-                else if (indexOpr == 2) result = a * b;
-                else result = a / b;
-
+            // проверяем являются ли числа арабскими, если оба арабские, то парсим
+            else if (isNum(A) && isNum(B)) {
+                a = Integer.parseInt(A);
+                b = Integer.parseInt(B);
+                isRoman = false;
             }
+            // если одно число арабское а другое римское бросаем соответствующее исключение
+            else if (a != 0 && isNum(B) || b != 0 && isNum(A)) throw new Exception(excMessages[1]);
+
+                // иначе бросаем общее исключение на несоответствие формату
+            else throw new Exception(excMessages[2]);
+
+            // проверяем чтобы числа на входе были не больше 10, если меньше то считаем
+            if (a > 10 || b > 10) throw new Exception(excMessages[3]);
+            else if (operator == operators[0]) result = a + b;
+            else if (operator == operators[1]) result = a - b;
+            else if (operator == operators[2]) result = a * b;
+            else result = a / b;
         }
-                                                                // если числа на входе были римские, то конвертируем результат обратно в римский
-        if (isRomanA) {                                         // и проверяем чтобы были не меньше 1
-            if (result < 1) throw new Exception("в римской системе нет отрицательных чисел и нуля");
-            else return arToRom(result);
+
+        // если числа на входе были римские, то конвертируем результат обратно в римский
+        if (isRoman) {
+            // проверяем чтобы результат был больше нуля иначе бросаем соответствуещее исключение
+            if (result > 0) return decimalToRoman(result);
+            else throw new Exception(excMessages[4]);
         } else return String.valueOf(result);
     }
 
-    // метод возвращает индекс массива String[] operators, в котором есть оператор из мат. выражения который принимается в параметрах
-    private static int getIndexOpr(String exp) {
+    /**
+     * Вспомогательные методы
+     **/
 
-        int mathOp = -1;
-
-        for (int i = 0; i < operators.length; i++) {
-
-            if (exp.matches(".*" + operators[i] + ".*")) mathOp = i;
+    private static boolean isNum(String str) {
+        if (str.isEmpty()) return false;
+        for (char c : str.toCharArray()) {
+            if (!Character.isDigit(c)) return false;
         }
-        return mathOp;
+        return true;
     }
 
-
-    // конвертирует римские числа в арабские
-    private static int romToAr(String rom) {
-
-        int ar = 0;
-
-        for (int i = 0; i < 10; i++) {
-
-            if (rom.equals(ramanNums[i])) {
-                ar = i + 1;
+    private static int romanToDecimal(String roman) {
+        int decimal = 0;
+        String rest = roman;
+        for (int i = 0; i < romanNums.length; i++) {
+            while (rest.startsWith(romanNums[i])) {
+                decimal += values[i];
+                rest = rest.substring(romanNums[i].length());
             }
         }
-        return ar;
+        if (roman.equals(decimalToRoman(decimal))) return decimal;
+        return 0;
     }
 
-    // конвертирует арабские числа в римские
-    private static String arToRom(int ar) {
-
-        String rom;
-
-        if (ar <= 10) rom = ramanNums[ar - 1];
-        else if (ar == 100) rom = ramanNums[18];
-        else {
-            rom = ramanNums[10 + (ar / 10) - 2] + (ar % 10 == 0 ? "" : ramanNums[(ar % 10) - 1]);
+    private static String decimalToRoman(int decimal) {
+        StringBuilder roman = new StringBuilder();
+        for (int i = 0; i < values.length; i++) {
+            while (decimal >= values[i]) {
+                decimal -= values[i];
+                roman.append(romanNums[i]);
+            }
         }
-        return rom;
+        return String.valueOf(roman);
     }
 }
